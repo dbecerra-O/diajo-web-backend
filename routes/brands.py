59dffbf7f9f3e98f6962f94ae95aee72a580ecea
dbecerra-O <dbecerraorellana@gmail.com>
2025-02-21 11:31:20 -0500

    add brand API for id

--- routes/brands.py
+++ routes/brands.py
@@ -26,3 +26,17 @@ async def get_brands(session: Session = Depends(get_session)):
         raise HTTPException(status_code=404, detail="Brands not found")
 
     return brands_list
+
+@brand.get("/diajosac/api/brands/{idBrand}", response_model=BrandSchema)
+async def get_brand(idBrand: int, session: Session = Depends(get_session)):
+    """
+    Endpoint para obtener una marca por su ID.
+    """
+    query = select(BrandModel).where(BrandModel.idBrand == idBrand)
+    result = session.execute(query)
+    brand_row = result.scalars().first()
+
+    if brand_row is None:
+        raise HTTPException(status_code=404, detail="Brand not found")
+
+    return brand_row
